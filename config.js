bf8a7667277d4612583d6c0cb5a2c5ccabf6aed2
dimliopeta <dimitra.lpt@gmail.com>
2024-12-18 16:39:05 +0200

    now students and professors are loaded from data.json when the server starts

--- config.js
+++ config.js
@@ -9,60 +9,85 @@ const db = mysql.createConnection({
     database: 'project_db'
 });
 
-// Φόρτωση δεδομένων από το αρχείο JSON
+// Μετατρέπουμε τα JSON objects σε Javascript objects και τα αποθηκεύουμε στο data
 const data = JSON.parse(fs.readFileSync('./public/json/data.json', 'utf8'));
 
 // Εισαγωγή δεδομένων στη βάση
 function insertData() {
     // Εισαγωγή καθηγητών
-    const professors = data.professors.map(prof => [
-        prof.name,
-        prof.surname,
-        prof.email,
-        prof.topic,
-        prof.landline,
-        prof.mobile,
-        prof.department,
-        prof.university,
-        'defaultpassword' // Προσωρινός κωδικός
-    ]);
-
-    db.query(
-        `INSERT INTO professors (name, surname, email, topic, landline, mobile, department, university, password) 
-        VALUES ?`,
-        [professors],
-        (err) => {
-            if (err) console.error('Error inserting professors:', err);
-            else console.log('Professors inserted successfully');
-        }
-    );
+    data.professors.forEach(professor => {
+        const query = `SELECT * FROM PROFESSORS WHERE email=?;`;
+        db.query(query, [professor.email], (err, results) => {
+            if (err) {
+                console.error('Error checking for duplicate professors: ', err);
+                return;
+            }
+            // Αν δεν υπάρχει, προσθήκη του καθηγητή
+            if (results.length === 0) {
+                const insertQuery = `
+                    INSERT INTO professors (name, surname, email, topic, landline, mobile, department, university, password)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+                `;
+                db.query(insertQuery, [
+                    professor.name,
+                    professor.surname,
+                    professor.email,
+                    professor.topic,
+                    professor.landline,
+                    professor.mobile,
+                    professor.department,
+                    professor.university,
+                    professor.password
+                ], (insertErr) => {
+                    if (insertErr) {
+                        console.error('Error inserting professor:', insertErr);
+                    } else {
+                        console.log(`Professor ${professor.email} added successfully.`);
+                    }
+                });
+            }
+        });
+    });
 
     // Εισαγωγή φοιτητών
-    const students = data.students.map(student => [
-        student.name,
-        student.surname,
-        student.student_number,
-        student.street,
-        student.number,
-        student.city,
-        student.postcode,
-        student.father_name,
-        student.landline_telephone,
-        student.mobile_telephone,
-        student.email,
-        'defaultpassword' // Προσωρινός κωδικός
-    ]);
-
-    db.query(
-        `INSERT INTO students (name, surname, student_number, street, number, city, postcode, father_name, landline_telephone, mobile_telephone, email, password) 
-        VALUES ?`,
-        [students],
-        (err) => {
-            if (err) console.error('Error inserting students:', err);
-            else console.log('Students inserted successfully');
-        }
-    );
+    data.students.forEach(student => {
+        const query = `SELECT * FROM STUDENTS WHERE email=?;`;
+        db.query(query, [student.email], (err, results) => {
+            if (err) {
+                console.error('Error checking for duplicate students:', err);
+                return;
+            }
+            if (results.length === 0) {
+                const insertQuery = `
+                    INSERT INTO students (name, surname, student_number, street, number, city, postcode, father_name, landline_telephone, mobile_telephone, email, password)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+                `;
+                db.query(insertQuery, [
+                    student.name,
+                    student.surname,
+                    student.student_number,
+                    student.street,
+                    student.number,
+                    student.city,
+                    student.postcode,
+                    student.father_name,
+                    student.landline_telephone,
+                    student.mobile_telephone,
+                    student.email,
+                    'defaultpassword' // Προσωρινός κωδικός
+                ], (insertErr) => {
+                    if (insertErr) {
+                        console.error('Error inserting student:', insertErr);
+                    } else {
+                        console.log(`Student ${student.email} added successfully.`);
+                    }
+                });
+            }
+        });
+    });
+    console.log(`Database up-to-date!`);
 }
+
 
 // Κλήση της συνάρτησης για εισαγωγή δεδομένων
 insertData();
