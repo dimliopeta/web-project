6e6cb82602041035daa8c82f5d1a31c7aba704d3
lazaros <lazaros.bazakas@gmail.com>
2025-01-05 16:34:20 +0200

    Got student navbar to function based on tab click

--- public/javascript/student_script.js
+++ public/javascript/student_script.js
@@ -1,3 +1,42 @@
+// Event listener για το navbar
+document.querySelectorAll('.nav-link').forEach(tab => {
+    tab.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Απόκρυψη όλων των sections
+        document.querySelectorAll('.content-section').forEach(section => {
+            section.style.display = 'none';
+        });
+
+        // Εμφάνιση του ενεργού section
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.style.display = 'block';
+
+        
+        }
+
+        // Ενημέρωση του active class στα tabs
+        document.querySelectorAll('.nav-link').forEach(link => {
+            link.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
+
+
+// Εμφάνιση του dashboard κατά την αρχική φόρτωση
+window.addEventListener('DOMContentLoaded', () => {
+    const defaultTab = document.querySelector('a[href="#dashboard"]');
+    if (defaultTab) {
+        defaultTab.click();
+    }
+});
+
+
+
+
 document.getElementById('logout-btn').addEventListener('click', (event) => {
     event.preventDefault(); // Αποφυγή της προεπιλεγμένης συμπεριφοράς του link
     fetch('/logout', {
